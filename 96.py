3d5850d870e215253a8aab710c72b8d903ced113
J. Aaron Gustafson <jag426@cornell.edu>
2015-07-23 23:49:11 -0700

    intelligent branching speeds up 96 by 2 orders of magnitude.

--- 96.py
+++ 96.py
@@ -1,51 +1,36 @@
-from functools import reduce
-from itertools import chain, product
+from itertools import product
 
 empty = '0'
 all_symbols = '123456789'
 
-def row(sudoku, r):
-    return sudoku[9*r:9*(r+1)]
+def neighborhood(sudoku, i):
+    row, col = i//9, i%9
+    box = row//3*3 + col//3
+    rowset = set(sudoku[row*9 : (row+1)*9])
+    colset = set([sudoku[r*9 + col] for r in range(9)])
+    boxset = set([sudoku[3*(box//3*9 + box%3) + r*9 + c] for r, c in product(range(3), repeat=2)])
+    return rowset | colset | boxset
 
-def col(sudoku, c):
-    return [sudoku[9*r+c] for r in range(9)]
+def best_branch(sudoku):
+    best_choices, best_index = set(all_symbols), None
+    for i in range(len(sudoku)):
+        if sudoku[i] is not empty:
+            continue
+        choices = set(all_symbols) - neighborhood(sudoku, i)
+        if len(choices) < len(best_choices):
+            best_choices, best_index = choices, i
+    return best_choices, best_index
 
-def box(sudoku, b):
-    br, bc = b//3, b%3
-    return [sudoku[3*(9*br+bc)+9*r+c] for r, c in product(range(3), repeat=2)]
-
-def check(sudoku):
-    def check_set(symbols):
-        seen = set()
-        for symbol in symbols:
-            if symbol is not empty:
-                if symbol in seen:
-                    return False
-                seen.add(symbol)
-        return True
-    for symbols in chain([row(sudoku, r) for r in range(9)],
-                         [col(sudoku, c) for c in range(9)],
-                         [box(sudoku, b) for b in range(9)]):
-        if not check_set(symbols):
-            return False
-    return True
-
-def solve(sudoku, i=0):
-    # if i is out of range, we're done
-    if i >= len(sudoku):
+def solve(sudoku):
+    choices, index = best_branch(sudoku)
+    if index is None:
         return sudoku
-    # if i is a given, move on
-    if sudoku[i] is not empty:
-        return solve(sudoku, i+1)
-    # try every possibility for i, recursively
-    for symbol in all_symbols:
-        sudoku[i] = symbol
-        if check(sudoku):
-            guess = solve(sudoku, i+1)
-            if guess:
-                return guess
-    # if none of them worked out, reset i and report the failure
-    sudoku[i] = empty
+    for symbol in choices:
+        sudoku[index] = symbol
+        solution = solve(sudoku)
+        if solution:
+            return solution
+    sudoku[index] = empty
     return None
 
 def parse(grid_string):
@@ -59,7 +44,8 @@ if __name__ == '__main__':
     # by header lines.
     input = open('p096_sudoku.txt').read().split('\n')[:-1]
     grid_strings = [input[i+1:i+10] for i in range(0, len(input), 10)]
-    for grid_string in grid_strings:
-        unsolved = parse(grid_string)
-        solved = solve(unsolved)
-        print(pretty(solved))
+    #for grid_string in grid_strings:
+    #    unsolved = parse(grid_string)
+    #    solved = solve(unsolved)
+    #    print(pretty(solved))
+    print(sum(map(lambda sol: int(''.join(sol[:3])), map(pretty, map(solve, map(parse, grid_strings))))))
