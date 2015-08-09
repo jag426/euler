b021602eceb8c74e399ee63c2fd50f06ecee8495
J. Aaron Gustafson <jag426@cornell.edu>
2015-08-08 23:54:30 -0700

    add prime factorization algorithm

--- util.py
+++ util.py
@@ -1,9 +1,10 @@
 def divisors(n):
     """A set of the proper divisors of n for any positive integer n.
        O(sqrt(n)).
+
     >>> divisors(1)
     {1}
-    >>> divisors(17)
+    >>> divisors(17.0)
     {1}
     >>> divisors(4) == {1, 2}
     True
@@ -11,6 +12,10 @@ def divisors(n):
     True
     >>> divisors(120) == {1, 2, 3, 4, 5, 6, 8, 10, 12, 15, 20, 24, 30, 40, 60}
     True
+    >>> len(divisors(360))
+    23
+    >>> len(divisors(2520))
+    47
     >>> divisors(0)
     Traceback (most recent call last):
         ...
@@ -22,15 +27,57 @@ def divisors(n):
     """
 
     from math import floor
-    if not n >= 1:
-        raise ValueError("n must be positive")
     if floor(n) != n:
         raise ValueError("n must be an integer")
+    n = floor(n)
+    if not n >= 1:
+        raise ValueError("n must be positive")
     divisors = {1}
-    for d in range(2, int(n**0.5)+1):
+    for d in range(2, floor(n**0.5)+1):
         if n%d == 0:
             divisors |= {d, n//d}
     return divisors
+
+def prime_factors(n):
+    """A sorted list of the prime factors of n for any positive integer n.
+
+    >>> prime_factors(1)
+    []
+    >>> prime_factors(2)
+    [2]
+    >>> prime_factors(17.0)
+    [17]
+    >>> prime_factors(360)
+    [2, 2, 2, 3, 3, 5]
+    >>> prime_factors(1234567890987654321)
+    [3, 3, 7, 19, 928163, 1111211111]
+    >>> prime_factors(0)
+    Traceback (most recent call last):
+        ...
+    ValueError: n must be positive
+    >>> prime_factors(2**0.5)
+    Traceback (most recent call last):
+        ...
+    ValueError: n must be an integer
+    """
+
+    from math import floor
+    if floor(n) != n:
+        raise ValueError("n must be an integer")
+    n = floor(n)
+    if not n >= 1:
+        raise ValueError("n must be positive")
+    if n == 1:
+        return []
+    prime_factors = []
+    d = 2
+    while d <= floor(n**0.5):
+        while n%d == 0:
+            prime_factors.append(d)
+            n //= d
+        d += 1
+    prime_factors.append(n)
+    return prime_factors
 
 if __name__ == "__main__":
     import doctest
