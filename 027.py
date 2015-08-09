cb8e178cee17dbdbdf293edd20fde8302fd3e2a0
J. Aaron Gustafson <jag426@cornell.edu>
2015-08-09 12:41:40 -0700

    simply optimize 27

--- 027.py
+++ 027.py
@@ -2,11 +2,12 @@ from util import is_prime
 
 max_primes = 0
 max_a, max_b = None, None
-for a in range(-999, 1000):
-    for b in range(2, 1000): # b must be prime, no use checking b<2
-        n = 0
-        while is_prime(n*n + n*a + b):
-            n += 1
-        if n > max_primes:
-            max_primes, max_a, max_b = n, a, b
+for b in range(2, 1000):
+    if is_prime(b):
+        for a in range(-999, 1000):
+            n = 0
+            while is_prime(n*n + n*a + b):
+                n += 1
+            if n > max_primes:
+                max_primes, max_a, max_b = n, a, b
 print(max_a*max_b)
